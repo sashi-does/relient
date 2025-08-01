b5bd865212197e640b7352d2f3accabb5863038f
sashi-does <sashiontop@gmail.com>
2025-08-01 12:20:35 +0530

    added demo payemnt

--- apps/web-app/app/api/dodo-payment/route.ts
+++ apps/web-app/app/api/dodo-payment/route.ts
@@ -11,54 +11,42 @@ export async function POST(req: Request) {
             return NextResponse.json({ error: 'Invalid event type' }, { status: 400 })
         }
 
-        if (body.type === 'payment.succeeded') {
-            const subscription = body.data
-            const { email, name } = subscription.customer
+        const subscription = body.data
+        const { email, name } = subscription.customer
 
+        let user = await prisma.user.findFirst({ where: { email } })
 
-            let user = await prisma.user.findFirst({ where: { email } })
-
-            if (!user) {
-
-                user = await prisma.user.create({
-                    data: {
-                        email,
-                        username: name,
-                        //   password: await hash(randomUUID(), 10), // hashed random string
-                        plan: 'PRO',
-                    },
-                })
-            }
-
-
-            const billing = await prisma.billing.create({
-                data: {
-                    city: subscription.billing.city,
-                    country: subscription.billing.country,
-                    state: subscription.billing.state,
-                    street: subscription.billing.street,
-                    zipcode: subscription.billing.zipcode,
-                },
-            })
-
-
-            await prisma.subscription.create({
-                data: {
-                    customerId: subscription.customer.customer_id,
-                    userId: user.id,
-                    paymentId: subscription.subscription_id,
-                    paymentType: 'CARD',
-                    nextBillingDate: new Date(subscription.next_billing_date),
-                    status: subscription.status,
-                    cardIssuingCountry: null,
-                    cardNetwork: null,
-                    billingId: billing.id,
-                    startDate: new Date(subscription.created_at),
-                },
-            })
-
-            return NextResponse.json({ message: 'Subscription recorded' }, { status: 200 })
+        if (!user) {
+            return NextResponse.json({ message: "Invalid User" }, { status: 400 })
         }
+
+        const billing = await prisma.billing.create({
+            data: {
+                city: subscription.billing.city,
+                country: subscription.billing.country,
+                state: subscription.billing.state,
+                street: subscription.billing.street,
+                zipcode: subscription.billing.zipcode,
+            },
+        })
+
+        await prisma.subscription.create({
+            data: {
+                customerId: subscription.customer.customer_id,
+                userId: user.id,
+                paymentId: subscription.subscription_id,
+                paymentType: 'CARD',
+                nextBillingDate: new Date(subscription.next_billing_date),
+                status: subscription.status,
+                cardIssuingCountry: null,
+                cardNetwork: null,
+                billingId: billing.id,
+                startDate: new Date(subscription.created_at),
+            },
+        })
+
+        return NextResponse.json({ message: 'Subscription recorded' }, { status: 200 })
+
     } catch (err) {
         console.error('Webhook error:', err)
         return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
